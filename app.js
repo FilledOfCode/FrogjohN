da0731d740967ac7ebe0163a1ac30babef7b73c4
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2020-12-15 18:28:10 +0000

    add basic tests

--- app.js
+++ app.js
@@ -13,4 +13,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('json spaces'
+app.set('json spaces', 4);
+
+app
