96803cf74794014fb75baba1fd2383f3b46bab59
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2020-12-19 13:17:49 +0000

    commit msg

--- app.js
+++ app.js
@@ -19,4 +19,4 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.
+app.use(express.static(path.join(__dirname, 
