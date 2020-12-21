7bac58b9354be9c10fa757bcaa6d35c4d8dacb01
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2020-12-21 16:38:27 +0000

    Regenerate gemspec for version 0.1.6

--- app.js
+++ app.js
@@ -21,4 +21,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRou
+app.use('/', indexRouter);
+ap
