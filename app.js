abec6ff2386382726bcc52248f43a4363310a718
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-01-06 08:26:49 +0000

    Setting up Travis CI

--- app.js
+++ app.js
@@ -33,4 +33,4 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.
+  res.locals.message = e
