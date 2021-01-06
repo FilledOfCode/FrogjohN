06321c52f65de557d9811f8950ca716f92e506be
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-01-06 14:19:14 +0000

    Changes made by Luiz Roland (luizherege@hotmail.com).

--- app.js
+++ app.js
@@ -33,4 +33,4 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = e
+  res.locals.message = err.message;
