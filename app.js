493eb39eb61ea605eb31e784011046c6f94484df
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-01-09 07:05:01 +0000

    version 0.5.1

--- app.js
+++ app.js
@@ -34,4 +34,4 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.
+  res.locals.error = req.app.get('en
