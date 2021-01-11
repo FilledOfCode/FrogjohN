eebd5b3c772806c3f4ff25a509c0892755cf25f2
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-01-11 08:47:28 +0000

    Enable NRT

--- app.js
+++ app.js
@@ -34,4 +34,6 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // ren
