dcee74170852b134dec73e1f7c73fcfdb2b06211
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-01-11 14:46:35 +0000

    add close button

--- app.js
+++ app.js
@@ -36,4 +36,4 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // ren
+  // render the erro
