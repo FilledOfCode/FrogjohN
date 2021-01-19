fa5145b357d93727c2984beacfef1ea2809f8a41
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-01-19 14:57:50 +0000

    v1.9

--- app.js
+++ app.js
@@ -37,4 +37,4 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.sta
+  res.status(err.status || 500)
