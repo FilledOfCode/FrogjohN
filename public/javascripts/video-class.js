b9ba0c7ee23994d22f936e0700a4374050df3cc5
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-05-18 21:16:43 +0000

    clarified spec

--- public/javascripts/video-class.js
+++ public/javascripts/video-class.js
@@ -3,3 +3,13 @@ const { execSync } = require('child_process');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 
+
+class Video {
+    /**
+     * @param {string} name name of the video without the path or file extention
+     */
+    constructor(name) {
+        this.name = name;
+        this.path = './public/images/' + this.name;
+        this.iframeJson = this.getIframeJson(); 
+    }
