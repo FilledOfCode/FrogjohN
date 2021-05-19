2639a3ab2b608a261d948a29fc81f85e39face27
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-05-19 06:16:48 +0000

    nuevo post

--- public/javascripts/video-class.js
+++ public/javascripts/video-class.js
@@ -13,3 +13,13 @@ class Video {
         this.path = './public/images/' + this.name;
         this.iframeJson = this.getIframeJson(); 
     }
+
+    get json() { 
+        return this.iframeJson;
+    }
+
+    getIframeJson = () => {
+        // ffprobe command to get video metadata in json format
+        const command = `"ffprobe" -show_frames -print_format json ${this.path}.mp4`
+        // run the command as a child process
+        const process = execSync(
