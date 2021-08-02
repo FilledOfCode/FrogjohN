9ddd46d6f9a7fb38da2700f0ad16aadbf2ab5899
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2021-08-02 22:49:26 +0000

    sh: fix format string bug in stack tracer

--- routes/videos.js
+++ routes/videos.js
@@ -6,4 +6,4 @@ const { Video } = require('../public/javascripts/video-class');
 // Get the json data for the video
 router.get('/:videoId.mp4/group-of-pictures.json', async function(req, res, next) {
   try {
-    const video = new Vid
+    const video = new Video(req.pa
