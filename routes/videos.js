a0a8887fc71763add37abde4d80b081497e642e6
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2022-07-30 19:06:04 +0000

    removed outdated comment

--- routes/videos.js
+++ routes/videos.js
@@ -27,4 +27,5 @@ router.get('/:videoName.mp4/group-of-pictures/:groupIndex.mp4', (req, res, next)
 });
 
 
-// Get html page with each group of pictures (playable mp4) and accompanying m
+// Get html page with each group of pictures (playable mp4) and accompanying metadata
+rout
